a557894932a0d8d2df0e759f47c274b2c35e7935
Mode-s <shota.umemoto7@gmail.com>
2026-04-30 15:16:50 +0900

    update responsive

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,21 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import icon from 'astro-icon';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://example.com',
+  integrations: [icon()],
+  build: {
+    assets: 'assets',
+  },
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          api: 'modern-compiler',
+        },
+      },
+    },
+  },
+});
